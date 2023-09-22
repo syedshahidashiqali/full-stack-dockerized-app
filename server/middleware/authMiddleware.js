cd454e1297a7b5bb6407517efe35cb7c1ebdfd61
Shahid Ashiq Ali <shahidashiqali@folio3.com>
2023-09-23 03:03:41 +0500

    remove consoles from the middleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -5,12 +5,6 @@ const config = process.env;
 exports.verifyToken = (req, res, next) => {
   const token =
     req.body.token || req.query.token || req.headers["authorization"];
-  // req.body.token || req.query.token || req.headers["x-access-token"];
-
-  console.log("10 => req.body.token", req.body.token);
-  console.log("11 => req.query.token", req.query.token);
-  console.log("12 => req.headers['Authorization']", req.headers["Authorization"]);
-  console.log("13 => req.headers", req.headers);
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
